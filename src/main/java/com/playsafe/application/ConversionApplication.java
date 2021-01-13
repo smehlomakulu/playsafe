be99dbfa05078667c32a17abca3dadd0d6a560dc
shadrack <shadrack@nrspos.co.za>
2021-01-13 12:49:14 +0200

    Fixes and pom file

--- src/main/java/com/playsafe/application/ConversionApplication.java
+++ src/main/java/com/playsafe/application/ConversionApplication.java
@@ -6,11 +6,13 @@
 package com.playsafe.application;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  *
  * @author shadrack
  */
+@SpringBootApplication
 public class ConversionApplication {
     
     public static void main(String []args){
