cf7780f558bd33bfd880180e99dca0a6e18718e1
shadrack <shadrack@nrspos.co.za>
2021-01-13 12:24:36 +0200

    Added logging and execution times with spring boot app

---
+++ src/main/java/com/playsafe/application/ConversionApplication.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.playsafe.application;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ *
+ * @author shadrack
+ */
+public class ConversionApplication {
+    
+    public static void main(String []args){
+        SpringApplication.run(ConversionApplication.class);        
+    }
+    
+}
