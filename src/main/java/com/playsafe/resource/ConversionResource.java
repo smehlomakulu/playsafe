15bbcacd59e3989ecd318cf783999f845b0043ea
shadrack <shadrack@nrspos.co.za>
2021-01-13 12:55:17 +0200

    Fixes

--- src/main/java/com/playsafe/resource/ConversionResource.java
+++ src/main/java/com/playsafe/resource/ConversionResource.java
@@ -3,8 +3,8 @@ package com.playsafe.resource;
 import com.playsafe.service.ConversionService;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,28 +20,28 @@ public class ConversionResource {
     @POST
     @Path("/ktoc")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response kelvinToCelcius(@QueryParam("kelvin") double kelvin) {
+    public Response kelvinToCelcius(@PathParam("kelvin") double kelvin) {
         return Response.ok(conversionService.kelvinToCelcius(kelvin)).build();
     }
     
     @POST
     @Path("/ctok")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response celciusToKelvin(@QueryParam("celcius") double celcius) {
+    public Response celciusToKelvin(@PathParam("celcius") double celcius) {
         return Response.ok(conversionService.celciusToKelvin(celcius)).build();
     }
     
     @POST
     @Path("/mtok")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response milesToKms(@QueryParam("miles") double miles) {
+    public Response milesToKms(@PathParam("miles") double miles) {
         return Response.ok(conversionService.milesToKm(miles)).build();
     }
     
     @POST
     @Path("/ktom")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response kmToMiles(@QueryParam("km") double km) {
+    public Response kmToMiles(@PathParam("km") double km) {
         return Response.ok(conversionService.kmToMiles(km)).build();
     }
 }
